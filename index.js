8aa61181b67b0e53fc5cc3adda4c29a9b3c5e9dc
bibinjaimon <bibinj@qburst.com>
2021-06-12 19:00:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import RootComponent from './src/main/root-component';
+
+AppRegistry.registerComponent(appName, () => RootComponent);
